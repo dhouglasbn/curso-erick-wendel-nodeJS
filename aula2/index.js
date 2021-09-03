1464f722a465d197ef45f5fed605da2a561f0ffa
dhouglasbn <dhouglasbn@gmail.com>
2021-09-03 16:59:00 -0300

    trying to get a phone number from a user

--- aula2/index.js
+++ aula2/index.js
@@ -3,3 +3,32 @@
  * 1. Preciso obet o número de telefone do meu usuário a partir de seu id
  * 2. Obter o endereço do usuário pelo id
  */
+
+function getUser() {
+    setTimeout(() => {
+        return {
+            id: 1,
+            name: "Aladin",
+            birthDate: new Date()
+        }
+    }, 1000)
+}
+
+function getPhone(userId) {
+    setTimeout(() => {
+        return {
+            number: "1199002",
+            ddd: 83
+        }
+    }, 2000);
+}
+
+function getAdress(userID) {
+
+}
+
+const user = getUser()
+const phoneNumber = getPhone(user.id)
+
+console.log(`User: ${user.name}`)
+console.log(`Phone number: ${phoneNumber.number}`)
