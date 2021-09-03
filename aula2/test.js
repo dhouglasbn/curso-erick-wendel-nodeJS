ce9f66802af3c30ce9c3eb7db26c963d7859aca2
dhouglasbn <dhouglasbn@gmail.com>
2021-09-03 18:04:48 -0300

    testing callback functions

---
+++ aula2/test.js
@@ -0,0 +1,77 @@
+const first_database = [
+    {
+        id: 0,
+        name: "dhouglas",
+        username: "dhouglasbn",
+        password: "dhouglas22"
+    },
+    {
+        id: 1,
+        name: " dhiego",
+        username: "dhiegocdbc",
+        password: "dhiego22"
+    },
+    {
+        id: 2,
+        name: "diego",
+        username: "diegosf",
+        password: "dlicinha"
+    },
+    {
+        id: 3,
+        name: "filipe",
+        username: "filipedeschamps",
+        password: "dlicinha"
+    },
+    {
+        id: 4,
+        name: "erick",
+        username: "wendelerick",
+        password: "mvpmicrosoft"
+    },
+    
+]
+
+const second_database = [
+    {
+        id: 0,
+        clothes: {
+            shirt: "preta",
+            pants: "marrom",
+            shoes: "preto"
+        }
+    },
+    {
+        id: 1,
+        clothes: {
+            shirt: "branca",
+            pants: "preta",
+            shoes: "preto"
+        }
+    },
+    {
+        id: 2,
+        clothes: {
+            shirt: "roxa",
+            pants: "preta",
+            shoes: "preto"
+        }
+    },
+    {
+        id: 3,
+        clothes: {
+            shirt: "laranja",
+            pants: "branca",
+            shoes: "verde"
+        }
+    },
+    {
+        id: 4,
+        clothes: {
+            shirt: "verde",
+            pants: "preta",
+            shoes: "preto"
+        }
+    },
+    
+]
