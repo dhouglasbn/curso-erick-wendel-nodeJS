711843030c1c1b17a932aca803f10ea13b2d4b49
dhouglasbn <dhouglasbn@gmail.com>
2021-09-03 17:49:58 -0300

    callback works

--- aula2/index.js
+++ aula2/index.js
@@ -14,26 +14,52 @@ function getUser(callback) {
     }, 1000)
 }
 
-function getPhone(userId) {
+function getPhone(userId, callback) {
     setTimeout(() => {
-        return {
-            number: "1199002",
+        return callback(null, {
+            number: "91199-0002",
             ddd: 83
-        }
+        })
     }, 2000);
 }
 
-function getAdress(userID) {
-
+function getAdress(userId, callback) {
+    setTimeout(() => {
+        return callback( null, {
+            street: "Dos bobos",
+            number: 0
+        })
+    }, 2000);
 }
 
 // callback() só vai retornar os dados da requisição após o tempo da execução
 
 getUser(function solveUser(error, user) {
+    // null || "" || 0 === false
     if (error) {
         console.error("DEU RUIM em USUARIO", error)
         return;
     }
+
+    getPhone(user.id, function solvePhone(error1, phone) {
+        if (error1) {
+            console.error("DEU RUIM em TELEFONE", error1)
+            return;            
+        }
+
+        getAdress(user.id, function solveAdress(error2, address) {
+            if (error2) {
+                console.error("DEU RUIM em ENDERECO", error2)
+                return;            
+            }
+
+            console.log(`
+            Nome: ${user.name}
+            Endereço: ${address.street}, ${address.number}
+            Telefone: (${phone.ddd}) ${phone.number}
+            `)
+        })
+    })
 })
 // const phoneNumber = getPhone(user.id)
 
