badff00a11e688b634fb8d5a9cde787c7dbbb3fc
dhouglasbn <dhouglasbn@gmail.com>
2021-09-04 10:47:20 -0300

    testing callback functions

--- aula2/test.js
+++ aula2/test.js
@@ -75,3 +75,45 @@ const second_database = [
     },
     
 ]
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        const data = first_database[id]
+
+        return callback(null, {
+            id: data.id,
+            name: data.name,
+            username: data.username
+        })
+    }, 1000);
+}
+
+function getClothes(userId, callback) {
+    setTimeout(() => {
+        const data = second_database[userId]
+
+        return callback(null, {
+            id: data.id,
+            shirt: data.clothes.shirt,
+            pants: data.clothes.pants,
+            shoes: data.clothes.shoes
+        })
+    }, 2000);
+}
+
+
+
+getUser(1, function solveUser(user_error, user) {
+    if (user_error) {
+        console.error("Deu ruim em USER", error)
+        return;
+    }
+    getClothes(user.id, function solveClothes(clothes_error, clothes) {
+        if (clothes_error) {
+            console.error("Deu ruim em ROUPAS", clothes_error)
+            return;
+        }
+        console.log(clothes)
+    })
+    console.log(user)
+})
