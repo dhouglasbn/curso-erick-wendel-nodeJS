5d5e6e3cbc174334135dca3894df4f88076a4cbe
dhouglasbn <dhouglasbn@gmail.com>
2021-09-21 10:06:47 -0300

    emmitting a lot of events, listening and logging

--- events/index.js
+++ events/index.js
@@ -6,6 +6,17 @@ class MyEmitter extends EventEmitter {
 
 const myEmitter = new MyEmitter()
 
-const eventName = "use:click"
+const eventName = "user:click"
 
+// escutando os eventos que ocorrerem em "user:click" e passando seus valores como função para dar log
 myEmitter.on(eventName, (click) => { console.log("Um usuário clicou", click);})
+
+// emitindo valores em "user:click"
+myEmitter.emit(eventName, "na barra de rolagem")
+
+let count = 0;
+
+setInterval(() => {
+    myEmitter.emit(eventName, "no ok " + (count++))
+    
+}, 1000);
