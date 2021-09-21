d619f48c440df54745e300416b67d73efcb7cc56
dhouglasbn <dhouglasbn@gmail.com>
2021-09-21 10:19:38 -0300

    using events at terminal

--- events/index.js
+++ events/index.js
@@ -14,9 +14,16 @@ myEmitter.on(eventName, (click) => { console.log("Um usuário clicou", click);})
 // emitindo valores em "user:click"
 myEmitter.emit(eventName, "na barra de rolagem")
 
-let count = 0;
+// let count = 0;
 
-setInterval(() => {
-    myEmitter.emit(eventName, "no ok " + (count++))
+// setInterval(() => {
+//     myEmitter.emit(eventName, "no ok " + (count++))
     
-}, 1000);
+// }, 1000);
+
+// trabalhar com processos no treminal dessa pasta
+const stdin = process.openStdin()
+
+stdin.addListener("data", value => {
+    console.log(`Você digitou ${value.toString().trim()}`);
+})
