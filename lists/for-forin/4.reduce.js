a81bb9aac6e2504018f708804c953b918486df06
dhouglasbn <dhouglasbn@gmail.com>
2021-09-22 10:53:01 -0300

    reducing all heights into a sum of heights

--- lists/for-forin/4.reduce.js
+++ lists/for-forin/4.reduce.js
@@ -3,7 +3,14 @@ const { getPeople } = require("./service");
 async function main() {
     try {
         const { results } = await getPeople("a");
+        const heights = results.map(item => parseInt(item.height))
+        console.log("heights: ", heights);
 
+        const total = heights.reduce((previous, next) => {
+            return previous + next;
+        })
+
+        console.log("total", total);
 
     } catch (error) {
         console.error("DEU RUIM", error);
