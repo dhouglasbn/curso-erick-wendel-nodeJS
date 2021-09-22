e1fb8ce5387cc9345c7605e6c9d4fd0750b5b1df
dhouglasbn <dhouglasbn@gmail.com>
2021-09-22 10:16:56 -0300

    filterring lars family

--- lists/for-forin/3.filter.js
+++ lists/for-forin/3.filter.js
@@ -1,1 +1,29 @@
-const service = require("./service");
+const { getPeople } = require("./service");
+
+async function main() {
+    try {
+        const { results } = await getPeople("a");
+
+        const larsFamily = results.filter(item => {
+            // por default precisa retornar um boolean
+            // para informar se deve manter ou remover da lista
+            // false -> remove da lista
+            // true -> mantém
+            // não encontrou -> retorna -1
+            // encontrou -> retorna a posição no array
+            
+            const result = item.name.toLowerCase().indexOf(`lars`) !== -1;
+            return result;
+        })
+
+        // retornando só o nome de cada pessoa
+        const names = larsFamily.map(person => person.name);
+
+        console.log(names);
+    } catch (error) {
+        console.error("DEU RUIM", error);
+    }
+
+}
+
+main()
