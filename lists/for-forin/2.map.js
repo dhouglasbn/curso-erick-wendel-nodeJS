8ba4008514113fc587ab1fd97ea79903d7dd9d77
dhouglasbn <dhouglasbn@gmail.com>
2021-09-22 09:02:52 -0300

    mapping with map function

--- lists/for-forin/2.map.js
+++ lists/for-forin/2.map.js
@@ -3,10 +3,20 @@ const service = require("./service");
 async function main() {
     try {
         const result = await service.getPeople("a");
-        let names = [];
-        result.results.forEach(item => {
-            names.push(item.name)
-        })
+        // const names = [];
+        // console.time("forEach")
+        // result.results.forEach(item => {
+        //     names.push(item.name)
+        // })
+        // console.timeEnd("forEach: ")
+
+        console.time("map")
+        // const names = result.results.map(function (person) {
+        //     return person.name
+        // })
+
+        const names = result.results.map(person => person.name)
+        console.timeEnd("map")
 
         console.log("names: ", names);
     } catch (error) {
