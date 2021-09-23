de8c250a959cc547e24de4da4d66b08d8d5b4ca2
dhouglasbn <dhouglasbn@gmail.com>
2021-09-23 11:08:59 -0300

    getting expected result with all information from a person

--- modulo03/01/service.js
+++ modulo03/01/service.js
@@ -5,6 +5,7 @@ const URL = `https://swapi.dev/api/people`;
 async function getPeople(name) {
     const url = `${URL}/?search=${name}&format=json`;
     const result = await get(url);
+    console.log(JSON.stringify(result.data));
     return result.data.results.map(personMapper);
 }
 
