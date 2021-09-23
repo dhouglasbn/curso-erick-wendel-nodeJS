db182d799ae7cafcd9735a17f67e72e54b7bee03
dhouglasbn <dhouglasbn@gmail.com>
2021-09-23 10:47:23 -0300

    connection finished, service gets swapi data

--- modulo03/01/service.js
+++ modulo03/01/service.js
@@ -1,7 +1,13 @@
 const { get } = require("axios");
 
-const URL = `https://swapi.dev/api/people`
+const URL = `https://swapi.dev/api/people`;
 
-async function getPeople() {
+async function getPeople(name) {
+    const url = `${URL}/?search=${name}&format=json`;
+    const result = await get(url);
+    return result.data;
+}
 
+module.exports = {
+    getPeople
 }
--- modulo03/01/test.js
+++ modulo03/01/test.js
@@ -1,1 +1,11 @@
 const assert = require("assert")
+const { getPeople } = require("./service")
+
+describe("Star Wars Test", () => {
+    it("deve buscar o r2d2 com o formato correto", async () => {
+        const expected = [{nome: "R2-D2", peso: "96"}]
+
+        const baseName = `r2-d2`
+        const result = await getPeople(baseName)
+    })
+})
