537f14e3014ddfc5b4e945cc44273161abe94476
dhouglasbn <dhouglasbn@gmail.com>
2021-09-24 10:08:17 -0300

    it reads heroes, test passed successfuly

--- modulo04/test.js
+++ modulo04/test.js
@@ -1,4 +1,5 @@
 const { deepEqual, ok } = require("assert");
+const database = require("./database");
 
 const DEFAUL_ITEM_REGISTER = {
     name: "Flash",
@@ -11,7 +12,11 @@ describe("Suite de manipulação de Heróis", () => {
     it("Deve pesquisar heróis usando arquivos", async () => {
         const expected = DEFAUL_ITEM_REGISTER;
 
-        ok(null, expected)
+        // isso é uma destructor, serve pra eu pegar o item respectivo no array
+        // ex: [item1 -> [0], item2 -> [1]]
+        const [result] = await database.list(expected.id)
+
+        deepEqual(result, expected)
     })
 
     // it("deve cadastrar um herói, usando arquivos", async() => {
