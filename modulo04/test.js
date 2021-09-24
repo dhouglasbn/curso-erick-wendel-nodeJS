95e5b266ddd66ce7d99eeefcb1239d00957108c2
dhouglasbn <dhouglasbn@gmail.com>
2021-09-24 09:30:23 -0300

    it should register a hero

---
+++ modulo04/test.js
@@ -0,0 +1,16 @@
+const { deepEqual } = require("assert");
+
+const DEFAUL_ITEM_REGISTER = {
+    name: "Flash",
+    power: "Speed",
+    id: 1
+}
+
+describe("Suite de manipulação de Heróis", () => {
+
+    it("deve cadastrar um herói, usando arquivos", async() => {
+        const expected = {}
+
+        ok(null, expected)
+    })
+})
