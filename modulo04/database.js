a24debe6c0f1658de7ce46662e56f5bb4ee8bf4b
dhouglasbn <dhouglasbn@gmail.com>
2021-09-24 10:35:47 -0300

    register hero function added

--- modulo04/database.js
+++ modulo04/database.js
@@ -1,9 +1,10 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs");
 const { promisify } = require("util");
 
 // readFile é uma função que pode demorar, por isso tranformamos ela em uma promise
 
 const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 
 // Outra forma de obter dados do json
 // const jsonData = require("./heroes.json");
@@ -22,8 +23,46 @@ class Database {
     }
 
 
-    writeFile() {
+    async writeFile(data) {
+        await writeFileAsync(this.FILE_NAME, JSON.stringify(data));
+        return true;
+    }
 
+    async register(hero) {
+        const data = await this.getFileData()
+        const id = hero.id <= 2 ? hero.id : Date.now();
+
+        /**
+         * Vai vir sem id
+         * {
+         * name: "Flash",
+         * power: "Speed"
+         * }
+         * // vai gerar o id
+         * {
+         * id: 283798217389
+         * }
+         * // vai juntar o id
+         * {
+         * name: "Flash",
+         * power: "Speed",
+         * id: 283798217389
+         * }
+         */
+
+        const heroWithId = {
+            id,
+            ...hero
+        }
+
+        // dados que já existiam em heroes.json + o novo herói
+        const finalData = {
+            ...data,
+            heroWithId
+        }
+
+        const result = await this.writeFile(finalData)
+        return result;  
     }
 
     async list(id) {
