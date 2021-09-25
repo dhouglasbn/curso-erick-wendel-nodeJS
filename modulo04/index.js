ae6118d76bb33a5f0c610808ddaf54dc5a94ef61
dhouglasbn <dhouglasbn@gmail.com>
2021-09-25 11:15:31 -0300

    list went successfuly, let's try to remove a hero

--- modulo04/index.js
+++ modulo04/index.js
@@ -9,9 +9,11 @@ async function main() {
         .version("v1")
         .option("-n, --name [value]", "Hero's Name")
         .option("-p, --power [value]", "Hero's Power")
+        .option("-i, --id [value]", "Hero's Id")
         
         .option("-r, --register", "Register a Hero")
         .option("-l, --list", "List a Heroes")
+        .option("-r, --remove [value]", "Remove a Hero")
         .parse(process.argv);
 
     const hero = new Hero(options)
@@ -27,6 +29,24 @@ async function main() {
             } 
 
             console.log("Hero registered successfuly!");
+        }
+
+        if(options.list) {
+            const result = await Database.list();
+
+            console.log(result);
+            return;
+        }
+
+        if(options.remove) {
+            const result = await Database.delete(hero.id);
+            
+            if(!result) {
+                console.error("Couldn't delete Hero");
+                return;
+            }
+
+            console.log("Hero removed successfuly!");
         }
     } catch (error) {
         console.error("DEU RUIM", error);
