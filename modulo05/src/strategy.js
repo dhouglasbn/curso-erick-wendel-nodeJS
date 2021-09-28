7fcbd4fb258c4d4c3f280db0ddc18bfe5ca52cce
dhouglasbn <dhouglasbn@gmail.com>
2021-09-28 10:43:49 -0300

    learned how work extends, static function & super()!

---
+++ modulo05/src/strategy.js
@@ -0,0 +1,7 @@
+class NotImplementedException extends Error {
+    // o super chama a constructor da classe que a gente deu extends
+    // extends é herdar todas as variáveis e funções da classe referida
+    constructor() {
+        super()
+    }
+}
